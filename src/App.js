85942221e0019fbadaf76fb308ba5c24ac54ed1c
venln4 <rizqi.ZULFIAN@uobgroup.com>
2022-03-27 11:30:18 +0700

    [AJB-002_ID01] - Seperate Components & Styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Fragment } from "react";
-import Header from "./components/Header/Header";
-import Table from "./components/Table/Table";
+import React from 'react';
+import Header from './modules/Header/Header';
+import Table from './modules/Table/Table';
 
 const App = () => {
   return (
